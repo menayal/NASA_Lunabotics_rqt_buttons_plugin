9510797d66b696b98cf83b889019816fa690124d
nayal_ubuntu <menayal1999@gmail.com>
2022-05-21 16:07:02 -0500

    Edited subscribertesting file

--- src/rqt_mypkg/posEncoder.py
+++ src/rqt_mypkg/posEncoder.py
@@ -7,10 +7,12 @@ from std_msgs.msg import String
 class PositionEncoder:
     def __init__(self):
         #instance variables here
-        self.isRunning = False
+        rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber('chatter', String, self.callback)
+        self.theData = "empty"
+
 
     def listener(self):
-        rospy.init_node('listener', anonymous=True)
         rospy.Subscriber('chatter', String, self.callback)
         rospy.spin()
 
@@ -20,7 +22,8 @@ class PositionEncoder:
     def callback(self, data):
         #rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
         print(data.data)
+        self.theData = str(data.data)
+        #return data.data
 
-if __name__ == '__main__':
-    pos = PositionEncoder()
-    pos.listener()
+# if __name__ == '__main__':
+#     pos = PositionEncoder()
